7f8d5130aed99a7232f2576280e6308f8433bf36
DefinetlyNotRay <edgarmasstudio@gmail.com>
2023-08-14 23:10:23 +0700

    ADDED LOGIN AND SIGN UP OMGOMGMIOGMG

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./*.{html}"],
   theme: {
+    colors: {
+      "blue-light": "#ACB1D6",
+      blue: "#8294C4",
+      white: "#DBDFEA",
+    },
+    boxShadow: {
+      L: "2px 2px 7px 0px #ACB1D6",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
