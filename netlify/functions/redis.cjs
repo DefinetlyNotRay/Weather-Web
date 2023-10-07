5035a76a160fcbfa1c3cca50d067c05d47e335ae
DefinetlyNotRay <edgarmasstudio@gmail.com>
2023-10-07 17:39:20 +0700

    Update redis.cjs

--- netlify/functions/redis.cjs
+++ netlify/functions/redis.cjs
@@ -9,11 +9,14 @@ exports.handler = async function (event) {
   console.log(event);
 
   const body = JSON.parse(event.body);
-  console.log(event.body);
   const locationInput = body.locationInput;
 
   const UPSTASH_API_KEY = "42808d40-2adf-4b76-b850-1e143ab417c8="; // Replace with your Upstash API key
   const UPSTASH_URL = "https://ideal-bluejay-31039.upstash.io";
+
+  console.log("UPSTASH_URL:", UPSTASH_URL);
+  console.log("UPSTASH_API_KEY:", UPSTASH_API_KEY);
+  console.log("locationInput:", locationInput);
 
   const response = await fetch(UPSTASH_URL, {
     method: "POST",
@@ -36,7 +39,3 @@ exports.handler = async function (event) {
     body: JSON.stringify({ message: "Data stored successfully" }),
   };
 };
-
-console.log("UPSTASH_URL:", UPSTASH_URL);
-console.log("UPSTASH_API_KEY:", UPSTASH_API_KEY);
-console.log("locationInput:", locationInput);
