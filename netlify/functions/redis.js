a62d38e6e2dd9485433b4064a2a7478072cfa797
DefinetlyNotRay <edgarmasstudio@gmail.com>
2023-10-07 16:51:09 +0700

    Update redis.js

--- netlify/functions/redis.js
+++ netlify/functions/redis.js
@@ -1,16 +1,26 @@
-const Redis = require("ioredis");
-const redis = new Redis({
-  host: "ideal-bluejay-31039.upstash.io",
-  port: 31039,
-  password: "8d1efb4c8a1f4202a56a4a04bef43f89",
-});
-
 exports.handler = async function (event) {
   const body = JSON.parse(event.body);
   const locationInput = body.locationInput;
-  const index = body.index;
 
-  await redis.set(`location:${index}`, locationInput);
+  const UPSTASH_API_KEY =
+    "AXk_ACQgZjU5N2RmNWUtN2I1YS00NWUwLWJkMzAtMjUxNTM3YzdhZmE0OGQxZWZiNGM4YTFmNDIwMmE1NmE0YTA0YmVmNDNmODk="; // Replace with your Upstash API key
+  const UPSTASH_URL = "https://ideal-bluejay-31039.upstash.io";
+
+  const response = await fetch(UPSTASH_URL, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${UPSTASH_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ location: locationInput }),
+  });
+
+  if (!response.ok) {
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({ message: "Error storing data" }),
+    };
+  }
 
   return {
     statusCode: 200,
