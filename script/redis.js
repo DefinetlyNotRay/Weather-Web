949c5cdabf7ac0e2ffaed06e7fef3ca03d1e8e3e
DefinetlyNotRay <edgarmasstudio@gmail.com>
2023-10-07 16:03:09 +0700

    Update redis.js

--- script/redis.js
+++ script/redis.js
@@ -5,9 +5,18 @@ const redis = new Redis({
   port: 31039,
   password: "8d1efb4c8a1f4202a56a4a04bef43f89",
 });
-
 let index = 1;
 let locationInput = document.querySelector(".location-input").value; // Get the value from the input element
+function red() {
+  console.log(locationInput);
+  redis.set(`location:${index++}`, locationInput);
+}
 
-console.log(locationInput);
-redis.set(`location:${index++}`, locationInput);
+themeBtn.addEventListener("click", red());
+searchBtn.addEventListener("click", red());
+
+function handleKeyDown(event) {
+  if (event.key === "Enter") {
+    red();
+  }
+}
